7ff278fe862d47aed534f0458c8b3f3792e11afb
Milena Romano <milena9919@gmail.com>
2024-02-21 23:46:29 +0000

    classes

---
+++ primeiro_exemplo/src/main/java/modelos/PessoaFisica.java
@@ -0,0 +1,12 @@
+package modelos;
+
+public class PessoaFisica extends Pessoa {
+    private int idade;
+    
+    public void setIdade (int idada){
+        this.idade = idade;
+    }
+public int getIdade() {
+    return this.idade; 
+}
+}
